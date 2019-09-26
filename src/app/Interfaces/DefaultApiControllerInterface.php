7af869ad0f4fc4721d0d8efd1547e622ee49897d
Marvin Roman <marvinroman@protonmail.com>
2019-09-25 22:07:55 -0700

    fixed interface

--- src/app/Interfaces/DefaultApiControllerInterface.php
+++ src/app/Interfaces/DefaultApiControllerInterface.php
@@ -1,53 +1,14 @@
 <?php 
 
-namespace \App\Interfaces;
+namespace App\Interfaces;
 
-use Slim\Http\Request;
-use Slim\Http\Response;
+use \Slim\Http\Request;
+use \Slim\Http\Response;
 
 interface DefaultApiControllerInterface 
 {
-    /**
-     * Read slot record(s)
-     *
-     * @param   Request     $request    Slim Request object
-     * @param   Response    $response   Slim Response object
-     * @param   Array       $args       Request params
-     *
-     * @return  Response                Slim Response Object
-     */
-    public function getRecord(Request $request, Response $response, $args);
-
-    /**
-     * Creates new slot record(s)
-     *
-     * @param   Request     $request    Slim Request object
-     * @param   Response    $response   Slim Response object
-     * @param   Array       $args       Request params
-     *
-     * @return  Response                Slim Response Object
-     */
-    public function postRecord(Request $request, Response $response, $args);
-
-    /**
-     * Delete existing slot record(s)
-     *
-     * @param   Request     $request    Slim Request object
-     * @param   Response    $response   Slim Response object
-     * @param   Array       $args       Request params
-     *
-     * @return  Response                Slim Response Object
-     */
-    public function deleteRecord(Request $request, Response $response, $args);
-
-    /**
-     * Update existing slot record(s)
-     *
-     * @param   Request     $request    Slim Request object
-     * @param   Response    $response   Slim Response object
-     * @param   Array       $args       Request params
-     *
-     * @return  Response                Slim Response Object
-     */
-    public function putRecord(Request $request, Response $response, $args);
+    public function getRecord(Request $request, Response $response, Array $args) : Response;
+    public function postRecord(Request $request, Response $response, Array $args) : Response;
+    public function deleteRecord(Request $request, Response $response, Array $args) : Response;
+    public function putRecord(Request $request, Response $response, Array $args) : Response;
 }
