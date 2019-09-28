0e75a768b902e07e0485b48fb5b722b98b1ae787
Marvin Roman <marvinroman@protonmail.com>
2019-09-28 02:29:15 -0700

    added Gui pages

--- src/app/Controllers/GuiController.php
+++ src/app/Controllers/GuiController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use \App\Models\User;
+use \App\Models\Slot;
 use \Slim\Views\Twig as View; 
 
 class GuiController extends Controller
@@ -9,30 +11,69 @@ class GuiController extends Controller
 
     public function index($request, $response, $args) 
     {
-        #TODO render blank page with menu
-        return $response->withJson([
-            'status'=> 'success', 
-            'message'=> 'Sites up.', 
-        ], 200);
+        $this->container->view->render($response, 'home.twig');
     }
 
     public function viewAppointments($request, $response, $args) 
     {
-        #TODO render page that lists appointments
+        $slots = Slot::whereNotNull('client_id')
+        ->selectRaw('
+        id,
+        order_id,
+        slot_begin,
+        stylist_id,
+        (SELECT name FROM users WHERE id = stylist_id) AS stylist_name,
+        client_id,
+        (SELECT name FROM users WHERE id = client_id) AS client_name
+        ')
+        ->get()
+        ;
+        $this->container->view->getEnvironment()->addGlobal('slots', $slots);
+        $this->container->view->render($response, 'appointments.twig');
     }
 
     public function viewLogs($request, $response, $args) 
     {
-        #TODO render page that lists logs
+        $logfile_path = __DIR__ . '/../../logs/api.log';
+        $logs = [];
+        if (file_exists($logfile_path)) {
+            $logfile = fopen($logfile_path,"r");
+        
+            while(! feof($logfile)) {
+                $logs[] = json_decode(fgets($logfile));
+            }
+    
+            fclose($logfile);
+            $this->container->view->getEnvironment()->addGlobal('logs', $logs);
+            $this->container->view->render($response, 'logs.twig');
+        } else {
+            $this->container->flash->addMessage('dark', "There currently aren't any logs to analyze.");
+            return $response->withRedirect($this->container->router->pathFor('home'));
+
+        }
     }
 
     public function viewSlots($request, $response, $args) 
     {
-        #TODO render page that lists slots
+        $slots = Slot::selectRaw('
+        id,
+        order_id,
+        slot_begin,
+        stylist_id,
+        (SELECT name FROM users WHERE id = stylist_id) AS stylist_name,
+        client_id,
+        (SELECT name FROM users WHERE id = client_id) AS client_name
+        ')
+        ->get()
+        ;
+        $this->container->view->getEnvironment()->addGlobal('slots', $slots);
+        $this->container->view->render($response, 'slots.twig');
     }
 
     public function viewUsers($request, $response, $args) 
     {
-        #TODO render page that lists users
+        $users = User::all();
+        $this->container->view->getEnvironment()->addGlobal('users', $users);
+        $this->container->view->render($response, 'users.twig');
     }
 }
