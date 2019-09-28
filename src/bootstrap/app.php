9d4ec16c91feeff2780974847adf3422f896a64a
Marvin Roman <marvinroman@protonmail.com>
2019-09-28 02:29:58 -0700

    added flash to container

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -47,6 +47,11 @@ $container['db'] = function ($container) use ($capsule) {
     return $capsule;
 };
 
+// add Slim Flash messages
+$container['flash'] = function ($container) {
+    return new \Slim\Flash\Messages;
+};
+
 // Setup Twig & add into Slim container
 $container['view'] = function ($container) {
     
@@ -65,7 +70,10 @@ $container['view'] = function ($container) {
     $base_css_path = pathinfo(__DIR__ . '/../css/app.css');
     $base_css_version = filemtime($_SERVER['DOCUMENT_ROOT'].$url);
     $view->getEnvironment()->addGlobal('base_css_version', $base_css_version);
-    
+        
+    // let the view have access to flash messages
+    $view->getEnvironment()->addGlobal('flash', $container->flash);
+
     return $view;
     
 };
